4450b29544a8ab33f67ddf8b299209406beadf0d
Kanhaiya Kumar <Kanhaiya5kumar@gmail.com>
2022-05-27 11:02:02 +0530

    java file modified

--- mainfile3.java
+++ mainfile3.java
@@ -5,6 +5,11 @@ public class mainfile3
 {
     public static void main(String[] args) 
     {
+        int [] arr={10,20,30,40,50,60};
+        for(int i=0;i<=10;i++)
+        {
+            System.out.println(i);
+        }
                 
     }
 }
