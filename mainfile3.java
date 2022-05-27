94b6ee63b804a1626208e744605f924ba3307d17
Kanhaiya Kumar <Kanhaiya5kumar@gmail.com>
2022-05-27 14:30:56 +0530

    Mouse Listener first commit

--- mainfile3.java
+++ mainfile3.java
@@ -1,21 +1,35 @@
 import java.util.Scanner;
 import java.awt.*;
+import java.lang.ProcessBuilder.Redirect;
+
 import javax.swing.*;
+
+import javafx.scene.paint.Color;
 public class mainfile3
 {
+    JFrame frame=new JFrame("Mouse Activity");
+    JPanel panel=new JPanel();
+    JLabel label=new JLabel("click here to Stop");
+    
+
+    public mainfile3()
+    {
+        frame.setSize(500,500);
+        frame.setDefaultCloseOperation(frame.EXIT_ON_CLOSE);
+        frame.setResizable(false);
+        frame.setLocationRelativeTo(null);
+        frame.add(panel);
+        frame.getContentPane().add(panel);
+        panel.setBackground(java.awt.Color.yellow);
+        panel.add(label);
+        label.setBounds(50,500,400,50);
+        label.setForeground(java.awt.Color.red);
+        label.setFont(new Font("arial",3,25));
+
+        frame.setVisible(true);
+    }
     public static void main(String[] args) 
     {
-        int [] arr={10,20,30,40,50,60};
-        for(int i=0;i<=10;i++)
-        {
-            System.out.print(i+",");
-        }
-        int k=25;
-        for(int j=k;j<=100;j++)
-        {
-            System.out.print(j+" ");
-            k++;
-        }
-                
+        new mainfile3();
     }
 }
