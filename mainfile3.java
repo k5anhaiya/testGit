fd96068bf74c534cadb7c707ced625a22aa7ad50
Kanhaiya Kumar <Kanhaiya5kumar@gmail.com>
2022-05-27 11:08:59 +0530

    java programme completed

--- mainfile3.java
+++ mainfile3.java
@@ -8,7 +8,13 @@ public class mainfile3
         int [] arr={10,20,30,40,50,60};
         for(int i=0;i<=10;i++)
         {
-            System.out.println(i);
+            System.out.print(i+",");
+        }
+        int k=25;
+        for(int j=k;j<=100;j++)
+        {
+            System.out.print(j+" ");
+            k++;
         }
                 
     }
