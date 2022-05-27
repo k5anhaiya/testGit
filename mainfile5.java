5167a84e385271ffc762b66e6570a08426072261
Kanhaiya Kumar <Kanhaiya5kumar@gmail.com>
2022-05-27 11:58:25 +0530

    System Time App completed

--- mainfile5.java
+++ mainfile5.java
@@ -1,11 +1,12 @@
 import javax.swing.*;
 import java.util.Scanner;
 import java.awt.*;
+import java.util.*;
 
 public class mainfile5 
 {
     JFrame fr=new JFrame("clock Demo");
-    JLabel la=new JLabel("bharat mahan hai");
+    JLabel la=new JLabel();
 
     public mainfile5()
     {
@@ -15,16 +16,40 @@ public class mainfile5
         fr.setDefaultCloseOperation(fr.EXIT_ON_CLOSE);
         fr.setLayout(new FlowLayout(FlowLayout.CENTER,150,100));
         fr.add(la);
-        setTime();
+        new TimeThread().start();
 
         fr.setVisible(true);
     }
     public void setTime()
     {
-        la.setFont(new Font("lucida calligraphy",Font.BOLD,25));
+        la.setFont(new Font("arial",Font.BOLD,25));
+        Date dt=new Date();
+        int h= dt.getHours();
+        int m=dt.getMinutes();
+        int s=dt.getSeconds();
+        //adding the time to the string using the ternary operator
+        String time=(h<10?"0"+h:h)+":"+(m<10?"0"+m:m)+":"+(s<10?"0"+s:s);
+        
+        la.setText(time);
+    }
+    class TimeThread extends Thread
+    {
+        public void run()
+        {
+            while(true)
+            {
+            setTime();
+            try 
+            {
+            sleep(1000); 
+            }
+            catch (Exception e) 
+            {}  
+        } 
+        }
     }
     public static void main(String[] args) 
     {
-        new mainfile5()
+        new mainfile5();
     }    
 }
