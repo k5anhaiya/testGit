1ccd697f58581c644429ed435b6aa4a4a988b0d6
Kanhaiya Kumar <Kanhaiya5kumar@gmail.com>
2022-05-27 11:33:31 +0530

    code written upto setfont

--- mainfile5.java
+++ mainfile5.java
@@ -1,3 +1,30 @@
-public class mainfile5 {
-    
+import javax.swing.*;
+import java.util.Scanner;
+import java.awt.*;
+
+public class mainfile5 
+{
+    JFrame fr=new JFrame("clock Demo");
+    JLabel la=new JLabel("bharat mahan hai");
+
+    public mainfile5()
+    {
+        fr.setSize(400,400);
+        fr.setLocationRelativeTo(null);
+        fr.setResizable(false);
+        fr.setDefaultCloseOperation(fr.EXIT_ON_CLOSE);
+        fr.setLayout(new FlowLayout(FlowLayout.CENTER,150,100));
+        fr.add(la);
+        la.setFont("lucida calligraphy",3,25);
+
+        fr.setVisible(true);
+    }
+    public void setTime()
+    {
+
+    }
+    public static void main(String[] args) 
+    {
+        
+    }    
 }
